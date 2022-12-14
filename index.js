1d38754bf1fcbb62a32f881b09e6a522f812d317
dannymaclaughlin <dannymaclaughlin@gmail.com>
2022-12-13 23:07:23 -0800

    Added functions to js file.
    - Added functions for add, subtract, multiply, and division.
    - Updated html file with skeleton and linked the css and js files.

---
+++ index.js
@@ -0,0 +1,27 @@
+// Your calculator is going to contain functions for all of the basic math operators 
+// you typically find on simple calculators, so start by creating functions for the 
+// following items and testing them in your browser’s console.
+
+// create a function for addition
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+};
+
+// create a function for subtraction
+function subtract(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+};
+
+// create a function for multiplication
+function multiply(num1, num2) {
+    let product = num1 * num2;
+    return product;
+};
+
+// create a function for division
+function divide(num1, num2) {
+    let quotent = num1 / num2;
+    return quotent;
+};
