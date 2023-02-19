7d9ad6a7899bb40ee6cb53e0064f1e1667b9b2bb
dannymaclaughlin <dannymaclaughlin@gmail.com>
2023-02-19 14:36:22 -0800

    Refactored code to be simpler to read.
    - Learned javascript dataset. Refactored HTML to include dataset to help distinguish between use for CSS or JS.
    - In JS file, I used switch statement for operators to make code more concise and easier to read.
    - Calculator will now successfully string together several operations and get the correct answer.

--- index.js
+++ index.js
@@ -1,250 +1,110 @@
-// Your calculator is going to contain functions for all of the basic math operators 
-// you typically find on simple calculators, so start by creating functions for the 
-// following items and testing them in your browser’s console.
+const clearButton = document.querySelector('[data-all-clear]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const operatorSignTextElement = document.querySelector('[data-operator-sign]');
 
-// create a function for addition
-const add = (num1, num2) => num1 + num2;
-
-// create a function for subtraction
-const subtract = (num1, num2) => num1 - num2;
-
-// create a function for multiplication
-const multiply = (num1, num2) => num1 * num2;
-
-// create a function for division
-const divide = (num1, num2) => num1 / num2;
-
-// Create a new function operate 
-// that takes an operator and 2 numbers
-// then calls one of the above functions on the numbers.
-const operate = (operator, num1, num2) => {
-    if (operator === '+') {
-        return add(num1, num2);
-    } else if (operator === '-') {
-        return subtract(num1, num2);
-    } else if (operator === '*') {
-        return multiply(num1, num2);
-    } else if (operator === '/') {
-        return divide(num1, num2)
-    } else {
-        return 'ERROR';
-    };
+// OPERATION FUNCTIONS
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
 };
 
-// Create the functions that populate the display when you click the number buttons. 
-//
-// You should be storing the ‘display value’ in a variable somewhere for use in the 
-// next step.
-
-// declaration variables
-let displayText = '0';
-let firstDisplayValue;
-let operator;
-let secondDisplayValue;
-let result;
-
-// digit buttons
-const button7 = document.querySelector('.digit7');
-button7.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '7';
-    } else {
-        displayText += '7';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button8 = document.querySelector('.digit8');
-button8.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '8';
-    } else {
-        displayText += parseInt('8');
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button9 = document.querySelector('.digit9');
-button9.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '9';
-    } else {
-        displayText += '9';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button4 = document.querySelector('.digit4');
-button4.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '4';
-    } else {
-        displayText += '4';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button5 = document.querySelector('.digit5');
-button5.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '5';
-    } else {
-        displayText += '5';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button6 = document.querySelector('.digit6');
-button6.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '6';
-    } else {
-        displayText += '6';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button1 = document.querySelector('.digit1');
-button1.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '1';
-    } else {
-        displayText += '1';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button2 = document.querySelector('.digit2');
-button2.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '2';
-    } else {
-        displayText += '2';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button3 = document.querySelector('.digit3');
-button3.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '3';
-    } else {
-        displayText += '3';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-const button0 = document.querySelector('.digit0');
-button0.addEventListener('click', () => {
-    if (displayText === '0') {
-        displayText = '0';
-    } else {
-        displayText += '0';
-    };
-
-    firstDisplayValue = parseInt(displayText);
-    document.getElementById('displayText').innerHTML = displayText;
-
-    displayText = displayText.substring(0, 10);
-});
-
-// decimal and clear buttons
-const decimalButton = document.querySelector('.decimal');
-decimalButton.addEventListener('click', () => {
-    if (!displayText.includes('.')) {
-        displayText += '.';
+function operate(operator, previousOperand, currentOperand) {
+    previousOperand = Number(previousOperand);
+    currentOperand = Number(currentOperand);
+    switch(operator) {
+        case '+':
+            return add(previousOperand, currentOperand);
+        case '-':
+            return subtract(previousOperand, currentOperand);
+        case '×':
+            return multiply(previousOperand, currentOperand);
+        case '÷':
+            return divide(previousOperand, currentOperand);
+        default:
+            return;
     }
+}
 
-    // firstDisplayValue = displayText;
-    document.getElementById('displayText').innerHTML = displayText;
+let currentOperand = '';
+let previousOperand = '';
+let operator = '';
+let result = ''
+
+// CALCULATOR FUNCTIONS
+function clear() {
+    currentOperand = '';
+    previousOperand = '';
+    operator = '';
+    updateDisplay();
+};
+
+function deleteDigit() {
+
+};
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand + number;
+};
+
+function updateDisplay() {
+    currentOperandTextElement.innerText = currentOperand;
+    previousOperandTextElement.innerText = previousOperand;
+    operatorSignTextElement.innerText = operator;
+};
+
+// EVENT LISTENERS
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNumber(button.innerText);
+        updateDisplay();
+        console.log(button.innerText);
+    });
 });
 
-const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-    if (displayText != '0') {
-        displayText = '0';
-    };
-
-    document.getElementById('displayText').innerHTML = displayText;
+    clear();
 });
 
-// operator buttons
-const additionButton = document.querySelector('.add');
-additionButton.addEventListener('click', () => {
-    operator = '+';
-    secondDisplayValue = firstDisplayValue;
-    firstDisplayValue = '';
-    displayText = '0'
-    result = operate(operator, secondDisplayValue, firstDisplayValue);
-})
-
-const subtractButton = document.querySelector('.subtract');
-subtractButton.addEventListener('click', () => {
-    operator = '-';
-    secondDisplayValue = firstDisplayValue;
-    firstDisplayValue = '';
-    displayText = '0';
-    result = operate(operator, secondDisplayValue, firstDisplayValue);
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (previousOperand != '' && currentOperand != '') {
+            result = operate(operator,previousOperand,currentOperand);
+            previousOperand = result;
+            currentOperand = '';
+            operator = button.innerText;
+            console.log(previousOperand);
+            updateDisplay();
+        } else if (previousOperand === '') {
+            operator = button.innerText;
+            previousOperand = currentOperand;
+            currentOperand = '';
+            console.log(operator);
+            updateDisplay();
+        }
+        
+        
+    });
 });
 
-const multiplyButton = document.querySelector('.multiply');
-multiplyButton.addEventListener('click', () => {
-    operator = '*';
-    secondDisplayValue = firstDisplayValue;
-    firstDisplayValue = ''
-    displayText = '0'
-    result = operate(operator, secondDisplayValue, firstDisplayValue);
-})
-
-const divideButton = document.querySelector('.divide');
-divideButton.addEventListener('click', () => {
-    operator = '/';
-    secondDisplayValue = firstDisplayValue;
-    firstDisplayValue = '';
-    displayText = '0';
-})
-
-const equalsButton = document.querySelector('.equals');
 equalsButton.addEventListener('click', () => {
-    result = operate(operator, secondDisplayValue, firstDisplayValue);
-    displayText = result;
-    firstDisplayValue = parseInt(result);
-
-    document.getElementById('displayText').innerHTML = result;
+    result = operate(operator,previousOperand,currentOperand);
+    console.log(result);
+    previousOperand = result;
+    currentOperand = '';
+    operator = '';
+    updateDisplay();
 })
